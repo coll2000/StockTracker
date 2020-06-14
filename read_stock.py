19f68a0d101b120d3ee49d272cc78c9298c5450f
coll2000 <31804171+coll2000@users.noreply.github.com>
2020-06-14 13:04:46 +0100

    Changed them to work with a selection of different currency pairs/stocks

--- read_stock.py
+++ read_stock.py
@@ -6,8 +6,18 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-df = pd.read_csv('GBPUSD.csv', parse_dates=True, index_col=0)
-# print(df.head())
 
-df[['Open', 'Close', 'High', 'Low']].plot()
+def read_stock_info(stock_name):
+    df = pd.read_csv('CSV/' + stock_name + '.csv', parse_dates=True, index_col=0)
+    # df['100ma'] = df['Close'].rolling(window=100, min_periods=0).mean()  # Takes a moving average
+    df[['Open', 'Close', 'High', 'Low']].plot()
+    print(df.head())
+
+    # ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+    # ax1.plot(df.index, df['Close'])
+    # ax1.plot(df.index, df['100ma'])
+
+
 plt.show()
+
+# plt.show()
--- write_stock.py
+++ write_stock.py
@@ -12,7 +12,19 @@ last_week_date = dt.datetime.today() - dt.timedelta(days=7)  # chooses a date 7 
 print("Today's Date: ", current_date)  # prints today's date
 print("This time last week: ", last_week_date)  # prints last week's date (7 days ago)
 
-df = web.DataReader('GBPUSD=X', 'yahoo', last_week_date, current_date)
-print(df.head(6))
 
-df.to_csv('GBPUSD.csv')
+def write_stock_info(stock_name):
+    stocks = ['GBPUSD=X',
+              'EURUSD=X',
+              'EURGBP=X',
+              'TSLA',
+              'BTC-USD']
+    if stock_name in stocks:
+        df = web.DataReader(stock_name, 'yahoo', last_week_date, current_date)
+        print(df.head())
+        df.to_csv('CSV/' + stock_name + '.csv')
+    else:
+        print("The code-name ", stock_name, " is not supported at the moment.")
+
+
+write_stock_info("TSLA")
