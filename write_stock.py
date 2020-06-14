70f843f0678a5e9f4a93e369cecffd0abec22d45
coll2000 <31804171+coll2000@users.noreply.github.com>
2020-06-14 12:34:04 +0100

    Changed the class name from "finance" to "write_stock" as it writes to the CSV. Only reads GDP/USD, need to add all currencies/FXs i want to work with

--- write_stock.py
+++ write_stock.py
@@ -6,7 +6,6 @@ import pandas_datareader.data as web
 
 style.use('ggplot')  # chooses the method of plotting 'ggplot'
 
-
 current_date = dt.datetime.today()  # gets the current date and time
 last_week_date = dt.datetime.today() - dt.timedelta(days=7)  # chooses a date 7 days before current day.
 
@@ -15,3 +14,5 @@ print("This time last week: ", last_week_date)  # prints last week's date (7 day
 
 df = web.DataReader('GBPUSD=X', 'yahoo', last_week_date, current_date)
 print(df.head(6))
+
+df.to_csv('GBPUSD.csv')
