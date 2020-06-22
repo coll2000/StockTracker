5f01819e9ae1a07a31279e32f3eaf4bfc29f156b
coll2000 <31804171+coll2000@users.noreply.github.com>
2020-06-22 15:42:38 +0100

    Has an interface. Can choose what type to get information of. Will print the information to the console, but does not write the information into the csv file.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import asksaveasfile
 
+import read_stock
 import write_stock
 
 win = tk.Tk()
@@ -10,7 +13,7 @@ coolFrame.pack()
 
 tk.Label(coolFrame, text="Choose Currency Pair").pack()
 
-listbox = tk.Listbox(win)
+listbox = tk.Listbox(win, width=30, height=15, selectmode=tk.EXTENDED)
 listbox.pack()
 
 currency_pairs = write_stock.get_currency_pairs()
@@ -22,10 +25,30 @@ for cp in currency_pairs:
 for s in stocks:
     listbox.insert(tk.END, s)
 
-def callback():
-    write_stock.write_stock_info()
 
-b = tk.Button(win, text="OK", command=callback())
+def callback():
+    selected_stock_name = listbox.get(listbox.curselection())
+
+    if listbox.get(listbox.curselection()):
+        stock_info = write_stock.write_stock_info(listbox.get(listbox.curselection()))
+        result = messagebox.askyesno("Complete!", "Do you wish to save " + selected_stock_name + ".csv ?")
+
+        if result:
+            save(stock_info, selected_stock_name)
+
+
+def save(stock_info, selected_stock_name):
+    contents = stock_info
+    new_file = asksaveasfile(parent=win, initialfile=selected_stock_name+".csv",
+                             title="Save file", defaultextension=".csv",
+                             filetypes=(("Comma-seperated Values (CSV) file", "*.csv"),))
+
+    if new_file:
+        new_file.write(str(contents))
+        new_file.close()
+
+
+b = tk.Button(win, text="OK", command=callback)
+b.pack()
 
 win.mainloop()
-
